dc84dd2cbca86ab28778e0b4aaab2856f1ccfcfe
Angel Shau <shauangel2000@gmail.com>
2021-11-03 14:37:17 +0800

    Initial Commit

---
+++ SwiftUI_eBook_Haikyu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_eBook_Haikyu
+//
+//  Created by 邵安祺 on 2021/11/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_eBook_Haikyu/SwiftUI_eBook_HaikyuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_eBook_HaikyuApp.swift
+//  SwiftUI_eBook_Haikyu
+//
+//  Created by 邵安祺 on 2021/11/3.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_eBook_HaikyuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
